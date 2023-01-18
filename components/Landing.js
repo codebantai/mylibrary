a07d2d0109d6ca419bb5c4a4ad4c2376bb9171be
Dell <vasanth@napses.com>
2023-01-18 11:19:19 +0530

    removed dark classnames

--- components/Landing.js
+++ components/Landing.js
@@ -25,7 +25,7 @@ export default function Landing({categories,pagination,setSearch,search,searchCa
           <div className="flex sm:w-3/5 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg">
             <ReactSelect
             instanceId="category-select"
-            className= "text-gray-900 text-sm bg-gray-50 rounded-l-lg border-r-gray-100 border-r-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:border-blue-500"
+            className= "text-gray-900 text-sm bg-gray-50 rounded-l-lg border-r-gray-100 border-r-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
             components={{
               IndicatorSeparator: () => null
             }}
@@ -39,7 +39,7 @@ export default function Landing({categories,pagination,setSearch,search,searchCa
               <input
                 type="search"
                 id="search-dropdown"
-                className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+                className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300  "
                 placeholder="Search"
                 required
                 value={search}
